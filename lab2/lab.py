7109af3e96f88c4c7b3e0d60c34dcd6a86da053c
Трунов Владимир Владимирович <vtrunov@ispras.ru>
2023-10-08 18:03:56 +0300

    Gauss added, Jacobi began

--- lab2/lab.py
+++ lab2/lab.py
@@ -1,12 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import copy 
 
 class Matrix:
-    data = []
-    hight = 0
-    width = 0
-
     def __init__(self, hight, width, elem_init):
+        self.data = []
         self.hight = hight
         self.width = width
 
@@ -18,7 +16,9 @@ class Matrix:
 
     def dump(self):
         for line in self.data:
-            print(line)
+            for elem in line:
+                print(format(elem, '.6f'), end=' ')
+            print()
     
     def findMaxInLine(self, line_number, start_row = 0):
         line = self.data[line_number]
@@ -52,26 +52,81 @@ class Matrix:
                 if (abs(self.data[i][j] - 0) < 1e-6):
                     self.data[i][j] = 0
 
+    def __mul__(self, column: list):
+        res = []
+        for i in range(self.hight):
+            elem = 0
+            for j in range(self.width):
+                elem += self.data[i][j] * column[j]
+
+            res.append(elem)
+
+        return res
+
     #def transpose
     #def calc_det
     #def calc_own_values
     #def calc_own_vectors
 
-
 # Gauss method
-def solveGauss(matrix: Matrix):
+def solveGauss(matrix_in: Matrix, right_side_in: list):
+    matrix = copy.deepcopy(matrix_in)
+    right_side = copy.deepcopy(right_side_in)
+
+    # Direct course
     for i in range(matrix.width):
         (max_row_elem, idx_of_max) = matrix.findMaxInRow(i, i)
         line_with_max_elem = matrix.data[idx_of_max]
+
+        matrix.data[i], matrix.data[idx_of_max] = matrix.data[idx_of_max], matrix.data[i]
+        right_side[i], right_side[idx_of_max] = right_side[idx_of_max], right_side[i]
 
         for j in range(i + 1, matrix.hight):
             curr_line = matrix.data[j];
             curr_line_elem_in_row = curr_line[i]
 
+            coef = curr_line_elem_in_row / max_row_elem
+            right_side[j] -= right_side[i] * coef
+            
             for k in range(i, matrix.width):
-                curr_line[k] -= line_with_max_elem[k] * (curr_line_elem_in_row / max_row_elem)
+                curr_line[k] -= line_with_max_elem[k] * coef
 
+    matrix.roundToZero()
+    
+    # Reverse course 
+    solutions = []
+    for i in range(matrix.hight - 1, 0 - 1, -1):
+        xi = right_side[i] / matrix.data[i][i]
+        solutions.insert(0, xi)
+        
+        for j in range(i, 0 - 1, -1):
+            right_side[j] -= matrix.data[j][i] * xi
+            matrix.data[j][i] = 0
+    
+    return solutions
+    
+def solveJacobi(matrix_in: Matrix, right_side: list):
+    # L- Lower triangular matrix
+    L = copy.deepcopy(matrix_in)
+    # D - Diagonal matrix
+    D = copy.deepcopy(matrix_in)
+    # U - Upper triangular matrix
+    U = copy.deepcopy(matrix_in)
+    
+    for i in range(matrix_in.hight):
+        for j in range(i, matrix_in.hight):
+            U.data[i][j] = 0
+            L.data[j][i] = 0
+
+            if (i != j):
+                D.data[i][j] = 0
+                D.data[j][i] = 0
+
+ 
 n = 3
+
 matrix = Matrix(n, n, lambda i, j: 1 * (j == i) + (1 / (j + i)) * (i != j))
-solveGauss(matrix)
-matrix.roundToZero()
+right_side = [1 / i for i in range(1, n + 1)]
+
+resGauss = solveGauss(matrix, right_side)
+resJacobi = solveJacobi(matrix, right_side)
