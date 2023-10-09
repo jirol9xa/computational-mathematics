376dc8c2721c8d1267758dd90873a01ca0f967e6
Трунов Владимир Владимирович <vtrunov@ispras.ru>
2023-10-09 12:43:25 +0300

    matrix methods improved

--- lab2/lab.py
+++ lab2/lab.py
@@ -3,22 +3,32 @@ import matplotlib.pyplot as plt
 import copy 
 
 class Matrix:
-    def __init__(self, hight, width, elem_init):
-        self.data = []
-        self.hight = hight
-        self.width = width
+    def __init__(self, *args): #hight, width, elem_init):
+        if len(args) > 1:
+            self.data = []
+            self.hight = args[0]
+            self.width = args[1]
 
-        for i in range(1, hight + 1):
-            line = []
-            for j in range(1, width + 1):
-                line.append(elem_init(i, j))
-            self.data.append(line)
+            for i in range(1, self.hight + 1):
+                line = []
+                for j in range(1, self.width + 1):
+                    line.append(args[2](i, j))
+                self.data.append(line)
+
+        elif len(args) == 1:
+            self.data = args[0]
+            self.hight = len(self.data)
+            self.width = len(self.data[0])
+        else:
+            raise ValueError("Can't init matrix with that pack or args")
+
 
     def dump(self):
         for line in self.data:
             for elem in line:
                 print(format(elem, '.6f'), end=' ')
             print()
+
     
     def findMaxInLine(self, line_number, start_row = 0):
         line = self.data[line_number]
@@ -32,6 +42,7 @@ class Matrix:
 
         return max_value, idx_of_max
 
+
     def findMaxInRow(self, row_number, start_line = 0):
         idx_of_max = start_line
         max_value = self.data[start_line][row_number];     
@@ -44,6 +55,7 @@ class Matrix:
 
         return max_value, idx_of_max
 
+
     # Method for supporting calculus of fixed accuracy
     # (round to 0 all elems less that 1e-6)
     def roundToZero(self):
@@ -52,18 +64,103 @@ class Matrix:
                 if (abs(self.data[i][j] - 0) < 1e-6):
                     self.data[i][j] = 0
 
-    def __mul__(self, column: list):
-        res = []
-        for i in range(self.hight):
-            elem = 0
-            for j in range(self.width):
-                elem += self.data[i][j] * column[j]
 
-            res.append(elem)
+    def __add__(self, other):
+        if self.hight != other.hight or self.width != other.width:
+            raise ValueError("It is not possible to add matrices with different sizes")
+        
+        res = copy.deepcopy(self)
+        for i in range(self.hight):
+            for j in range(self.width):
+                res.data[i][j] += other.data[i][j]
 
         return res
 
-    #def transpose
+
+    def __mul__(self, other):
+        if isinstance(other, list):
+            res = []
+            for i in range(self.hight):
+                elem = 0
+                for j in range(self.width):
+                    elem += self.data[i][j] * other[j]
+
+                res.append(elem)
+
+            return res
+        
+        elif isinstance(other, int):
+            res = copy.deepcopy(self);
+            for i in range(res.hight):
+                for j in range(res.width):
+                    res.data[i][j] *= other
+
+            return res
+
+        if self.width != other.hight:
+            raise ValueError("Can calculate the product of matrices only if \
+                             the number of columns of the first matrix is equal \
+                             to the number of rows of the second")
+        
+        res = Matrix(self.hight, other.width, lambda i, j: 0)
+
+        for i in range(self.hight):
+            for j in range(other.width):
+                for k in range(self.width):
+                    res.data[i][j] += self.data[i][k] * other.data[k][j]
+
+        return res
+
+    
+    def __pow__(self, power):
+        if power < -1:
+            raise ValueError("Power of matrix should be >= 1")
+        elif power == -1:
+            return self.getInversed()
+
+        res = copy.deepcopy(self)
+        for i in range(power - 1):
+            res = res * self
+
+        return res
+
+
+    def getTransposed(self):
+        res = copy.deepcopy(self)
+
+        for i in range(res.hight):
+            for j in range(res.width):
+                res.data[i][j], res.data[j][i] = res.data[j][i], res.data[i][j]
+        return res
+
+    
+    def getInversed(self):
+        matrix = copy.deepcopy(self)
+
+        augmented_matrix = [
+            [
+                matrix.data[i][j] if j < matrix.hight else int(i == j - matrix.hight)
+                for j in range(2 * matrix.hight)
+            ]
+            for i in range(matrix.hight)
+        ]
+        for i in range(matrix.hight):
+            pivot = augmented_matrix[i][i]
+            if pivot == 0:
+                raise ValueError("Matrix is not invertible")
+            for j in range(2 * matrix.hight):
+                augmented_matrix[i][j] /= pivot
+            for j in range(matrix.hight):
+                if i != j:
+                    scalar = augmented_matrix[j][i]
+                    for k in range(2 * matrix.hight):
+                        augmented_matrix[j][k] -= scalar * augmented_matrix[i][k]
+        inverse = [
+            [augmented_matrix[i][j] for j in range(matrix.hight, 2 * matrix.hight)]
+            for i in range(matrix.hight)
+        ]
+        return Matrix(inverse)
+
     #def calc_det
     #def calc_own_values
     #def calc_own_vectors
@@ -122,8 +219,14 @@ def solveJacobi(matrix_in: Matrix, right_side: list):
                 D.data[i][j] = 0
                 D.data[j][i] = 0
 
+    R = (D ** (-1)) * (-1) * (L + U)
+    F = (D ** (-1)) * right_side
+    
+
  
 n = 3
+
+
 
 matrix = Matrix(n, n, lambda i, j: 1 * (j == i) + (1 / (j + i)) * (i != j))
 right_side = [1 / i for i in range(1, n + 1)]
